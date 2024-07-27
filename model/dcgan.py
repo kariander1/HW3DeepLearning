d6c875e142fc8326e5a0fbad0cf43cc75a0b56b1
Shai Yehezkel <shai444@gmail.com>
2024-07-27 23:18:05 +0300

    Finilize

--- model/dcgan.py
+++ model/dcgan.py
@@ -1,14 +1,34 @@
 import torch
 import torch.nn as nn
-from .gan_base import GANBase
+import torch.optim as optim
 
-class DCGAN(GANBase):
+
+class DCGAN():
     def __init__(self, nz, lr, device, channels=3):
         generator = self.Generator(nz, channels)
         discriminator = self.Discriminator(channels)
         criterion = nn.BCELoss()
-        super().__init__(generator, discriminator, nz, lr, device, criterion=criterion)
+        
+        self.device = device
+        self.generator = generator.to(device)
+        self.discriminator = discriminator.to(device)
+        self.criterion = criterion
 
+        self.opt_discriminator = optim.Adam(self.discriminator.parameters(), lr=lr, betas=(0.5, 0.999)) # Same as in paper
+        self.opt_generator = optim.Adam(self.generator.parameters(), lr=lr, betas=(0.5, 0.999)) # Same as in paper
+        
+        # Apply the weights_init function to the model
+        self.generator.apply(self.weights_init)
+        self.discriminator.apply(self.weights_init)
+        
+    def weights_init(self, m):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+        elif classname.find('BatchNorm') != -1:
+            nn.init.normal_(m.weight.data, 1.0, 0.02)
+            nn.init.constant_(m.bias.data, 0)
+            
     class Generator(nn.Module):
         def __init__(self, nz, channels):
             super().__init__()
@@ -48,29 +68,30 @@ class DCGAN(GANBase):
         def forward(self, input):
             return self.main(input).view(-1)
 
+                
     def train_discriminator(self, real_data, fake_data):
-        self.netD.zero_grad()
+        self.discriminator.zero_grad()
         batch_size = real_data.size(0)
         labels_real = torch.full((batch_size,), 1, dtype=torch.float, device=self.device)
         labels_fake = torch.full((batch_size,), 0, dtype=torch.float, device=self.device)
 
-        output_real = self.netD(real_data)
-        errD_real = self.criterion(output_real, labels_real)
-        errD_real.backward()
+        output_real = self.discriminator(real_data)
+        err_disc_real = self.criterion(output_real, labels_real)
+        err_disc_real.backward()
 
-        output_fake = self.netD(fake_data.detach())
-        errD_fake = self.criterion(output_fake, labels_fake)
-        errD_fake.backward()
+        output_fake = self.discriminator(fake_data.detach())
+        err_disc_fake = self.criterion(output_fake, labels_fake)
+        err_disc_fake.backward()
 
-        self.optimizerD.step()
-        return errD_real + errD_fake
+        self.opt_discriminator.step()
+        return err_disc_real + err_disc_fake
 
     def train_generator(self, fake_data):
-        self.netG.zero_grad()
+        self.generator.zero_grad()
         batch_size = fake_data.size(0)
         labels = torch.full((batch_size,), 1, dtype=torch.float, device=self.device)
-        output = self.netD(fake_data)
-        errG = self.criterion(output, labels)
-        errG.backward()
-        self.optimizerG.step()
-        return errG
+        output = self.discriminator(fake_data)
+        err_gen = self.criterion(output, labels)
+        err_gen.backward()
+        self.opt_generator.step()
+        return err_gen
--- model/wgan.py
+++ model/wgan.py
@@ -2,29 +2,32 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-class GANBase(nn.Module):
-    def __init__(self, generator, discriminator, nz, lr, device, clamp_value=0.01, n_critic=5):
-        super().__init__()
-        self.netG = generator.to(device)
-        self.netD = discriminator.to(device)
-        self.nz = nz
-        self.lr = lr
-        self.device = device
-        self.clamp_value = clamp_value
-        self.n_critic = n_critic
-        self.optimizerG = optim.RMSprop(self.netG.parameters(), lr=lr)
-        self.optimizerD = optim.RMSprop(self.netD.parameters(), lr=lr)
-
-    def clamp_weights(self):
-        for p in self.netD.parameters():
-            p.data.clamp_(-self.clamp_value, self.clamp_value)
-
-class WGAN(GANBase):
-    def __init__(self, nz, lr, device, clamp_value=0.01, n_critic=5, channels=1):
+class WGAN():
+    def __init__(self, nz, lr, device, lambda_gp=10, clamp_value=0.01, n_critic=5, channels=1):
         generator = self.Generator(nz, channels)
         discriminator = self.Discriminator(channels)
-        super().__init__(generator, discriminator, nz, lr, device, clamp_value=clamp_value, n_critic=n_critic)
+        
+        self.device = device
+        self.generator = generator.to(device)
+        self.discriminator = discriminator.to(device)
+        self.clamp_value = clamp_value
+        self.n_critic = n_critic
+        self.lambda_gp = lambda_gp
+        self.opt_discriminator = optim.RMSprop(self.discriminator.parameters(), lr=lr)
+        self.opt_generator = optim.RMSprop(self.generator.parameters(), lr=lr)
 
+        # Apply the weights_init function to the model
+        self.generator.apply(self.weights_init)
+        self.discriminator.apply(self.weights_init)
+        
+    def weights_init(self, m):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+        elif classname.find('BatchNorm') != -1:
+            nn.init.normal_(m.weight.data, 1.0, 0.02)
+            nn.init.constant_(m.bias.data, 0)
+            
     class Generator(nn.Module):
         def __init__(self, nz, channels):
             super().__init__()
@@ -63,52 +66,58 @@ class WGAN(GANBase):
         def forward(self, input):
             return self.main(input).view(-1)
 
+    def clamp_weights(self):
+        if self.clamp_value is not None:
+            for p in self.discriminator.parameters():
+                p.data.clamp_(-self.clamp_value, self.clamp_value)
+
+    def gradient_penalty(self, real_data, fake_data):
+        batch_size = real_data.size(0)
+        epsilon = torch.rand(batch_size, 1, 1, 1, device=self.device, requires_grad=True)
+        interpolates = epsilon * real_data + (1 - epsilon) * fake_data
+        interpolates = interpolates.to(self.device)
+        interpolates.requires_grad_(True)
+
+        d_interpolates = self.discriminator(interpolates)
+        
+        fake = torch.ones(d_interpolates.size(), device=self.device, requires_grad=False)
+        
+        gradients = torch.autograd.grad(
+            outputs=d_interpolates,
+            inputs=interpolates,
+            grad_outputs=fake,
+            create_graph=True,
+            retain_graph=True,
+            only_inputs=True
+        )[0]
+        
+        gradients = gradients.view(batch_size, -1)
+        gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+        
+        return gradient_penalty
+    
     def train_discriminator(self, real_data, fake_data):
-        self.netD.zero_grad()
-        output_real = self.netD(real_data)
+        self.discriminator.zero_grad()
+        
+        output_real = self.discriminator(real_data)
         D_real = output_real.mean()
-        D_real.backward()
-
-        output_fake = self.netD(fake_data.detach())
+        
+        output_fake = self.discriminator(fake_data.detach())
         D_fake = output_fake.mean()
-        D_fake.backward()
-
-        self.optimizerD.step()
-        self.clamp_weights()
-        return D_real - D_fake
+        
+        gradient_penalty = self.gradient_penalty(real_data, fake_data.detach())
+        
+        D_loss = D_fake - D_real + self.lambda_gp * gradient_penalty
+        D_loss.backward()
+        
+        self.opt_discriminator.step()
+        
+        return D_loss
 
     def train_generator(self, fake_data):
-        self.netG.zero_grad()
-        output = self.netD(fake_data)
+        self.generator.zero_grad()
+        output = self.discriminator(fake_data)
         G_loss = -output.mean()
         G_loss.backward()
-        self.optimizerG.step()
+        self.opt_generator.step()
         return G_loss
-
-if __name__ == "__main__":
-    # Hyperparameters
-    nz = 100
-    lr = 0.0002
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    clamp_value = 0.01
-    n_critic = 5
-    channels = 3
-    batch_size = 64
-    image_size = 28
-
-    # Initialize WGAN model
-    wgan = WGAN(nz, lr, device, clamp_value, n_critic, channels)
-
-    # Create a batch of real data (e.g., random noise for simplicity here)
-    real_data = torch.randn(batch_size, channels, image_size, image_size, device=device)
-
-    # Create a batch of fake data
-    noise = torch.randn(batch_size, nz, 1, 1, device=device)
-    fake_data = wgan.netG(noise)
-
-    # Training step
-    for _ in range(n_critic):
-        d_loss = wgan.train_discriminator(real_data, fake_data)
-    g_loss = wgan.train_generator(fake_data)
-
-    print(f"D_loss: {d_loss.item()}, G_loss: {g_loss.item()}")
